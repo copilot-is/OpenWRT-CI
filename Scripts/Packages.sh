a873254e2b9e5b25121f62a05b8f6cf8965bbba7
John <jun0205@live.com>
2025-09-19 15:20:14 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -118,8 +118,8 @@ UPDATE_VERSION() {
 
 
 
-#不编译xray-core
-#sed -i 's/+xray-core//' luci-app-passwall/Makefile
+#编译xray-core
+sed -i 's/+xray-core//' luci-app-passwall/Makefile
 
 #删除官方的默认插件
 rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypass*}
